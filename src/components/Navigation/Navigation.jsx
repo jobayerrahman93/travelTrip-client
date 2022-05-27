48f20b5bedf29d3fb8543e8d22355fb6fbf45902
jobayercse71 <jobayercse71.com>
2022-05-27 22:08:14 +0600

    setup-project

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navigation.css';
+
+const Navigation = () => {
+    return (
+        <div>
+
+            <h1>Navigation</h1>
+
+        </div>
+    );
+};
+
+export default Navigation;
